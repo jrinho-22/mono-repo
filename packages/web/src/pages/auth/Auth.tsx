2736718092248dd3a9a2c133cdbe40845cbc931c
Junior <jrinho22@gmail.com>
2024-04-16 21:02:01 -0300

    login fixes

--- packages/web/src/pages/auth/Auth.tsx
+++ packages/web/src/pages/auth/Auth.tsx
@@ -1,139 +1,167 @@
-import React, { useState } from 'react'
-import { signInWithGoogle, getLoggedUser, emailPasswordSignIn, emailPasswordSignUp } from '@monorepo/axios-config'
-import { Button, Input, Requirements  } from '@monorepo/components'
-import { Box, Link } from './Auth.styled'
-import { useDispatch, useSelector } from 'react-redux';
-import { authActions } from '../../redux/reducers/auth'
-import GoogleButton from 'react-google-button'
+import React, { useState } from "react";
+import {
+  signInWithGoogle,
+  getLoggedUser,
+  emailPasswordSignIn,
+  emailPasswordSignUp
+} from "@monorepo/axios-config";
+import { Button, Input, Requirements } from "@monorepo/components";
+import { Box, Link } from "./Auth.styled";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../../redux/reducers/auth";
+import GoogleButton from "react-google-button";
 
 const passwordRequirements = [
   {
-    text: 'Must be at least 6 characters',
-    validator: val => val.length >= 6,
+    text: "Must be at least 6 characters",
+    validator: (val) => val.length >= 6
   },
   {
-    text: 'Must contain at least one number',
-    validator: val => /\d/g.test(val),
+    text: "Must contain at least one number",
+    validator: (val) => /\d/g.test(val)
   }
 ];
-
 
 export const Login: React.FC = () => {
   const dispatch = useDispatch();
 
-  const [valid,  setValid] = useState(false);
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+  const [valid, setValid] = useState(false);
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
   const googleSignIn = async () => {
-    const key = await signInWithGoogle()
-    const loggedUser = await getLoggedUser(key.name)
-    dispatch(authActions.login(loggedUser))
-  }
+    const key = await signInWithGoogle();
+    const loggedUser = await getLoggedUser(key.name);
+    if (loggedUser) {
+      dispatch(authActions.login(loggedUser));
+    } else dispatch(authActions.login(key));
+  };
 
   const handleSignIn = async () => {
-    const loggedUser = await emailPasswordSignIn(username, password)
+    const loggedUser = await emailPasswordSignIn(username, password);
     if (loggedUser) {
-      dispatch(authActions.login({name: loggedUser}))
+      dispatch(authActions.login({ name: loggedUser }));
     }
   };
 
   const switchSignUpPage = () => {
-    dispatch(authActions.signUp())
+    dispatch(authActions.signUp());
   };
 
-
-  return ( 
-  <React.Fragment>
-    <h1 style={{color: '#5b5b5b'}}>Sign In With</h1>
-    <Input placeholder='Email' value={username} onChange={e => setUsername(e.target.value)} />
-    <Input placeholder='Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
-    <Requirements
-      value={password}
-      requirements={passwordRequirements}
-      onValidChange={isValid => setValid(isValid)}
-    />
-
-    <Button
-      disabled={!valid || !username}
-      sx={{
-        margin: '15px 0px'
-      }}
-      text='sign In'
-      onClick={handleSignIn}
-    />
-    <span style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontFamily: 'Arial',
-      fontSize: '13px'
-      }}>
-      Sign in with Google or&nbsp; <Link onClick={switchSignUpPage}>sign up</Link>
-    </span>
-    <GoogleButton style={{margin: '4px'}} onClick={googleSignIn}> google sigin </GoogleButton>
-  </React.Fragment>
-  )
-}
-
-const SignUp: React.FC = () => {
-  const dispatch = useDispatch();
-
-  const [valid,  setValid] = useState(false);
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
-
-  const handleSignUp = async () => {
-    const loggedUser = await emailPasswordSignUp(username, password)
-    if (loggedUser) {
-      dispatch(authActions.login({name: loggedUser.email}))
-    }
-  };
-
-  const switchSignInPage = () => {
-    dispatch(authActions.signIn())
-  };
-
-  return ( 
+  return (
     <React.Fragment>
-      <h1 style={{color: '#5b5b5b'}}>Sign Up</h1>
-      <Input placeholder='Email' value={username} onChange={e => setUsername(e.target.value)} />
-      <Input placeholder='Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+      <h1 style={{ color: "#5b5b5b" }}>Sign In With</h1>
+      <Input
+        placeholder="Email"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <Requirements
         value={password}
         requirements={passwordRequirements}
-        onValidChange={isValid => setValid(isValid)}
+        onValidChange={(isValid) => setValid(isValid)}
       />
 
       <Button
         disabled={!valid || !username}
         sx={{
-          margin: '15px 0px'
+          margin: "15px 0px"
         }}
-        text='sign Up'
+        text="sign In"
+        onClick={handleSignIn}
+      />
+      <span
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontFamily: "Arial",
+          fontSize: "13px"
+        }}
+      >
+        Sign in with Google or&nbsp;{" "}
+        <Link onClick={switchSignUpPage}>sign up</Link>
+      </span>
+      <GoogleButton style={{ margin: "4px" }} onClick={googleSignIn}>
+        {" "}
+        google sigin{" "}
+      </GoogleButton>
+    </React.Fragment>
+  );
+};
+
+const SignUp: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const [valid, setValid] = useState(false);
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const handleSignUp = async () => {
+    const loggedUser = await emailPasswordSignUp(username, password);
+    if (loggedUser) {
+      dispatch(authActions.login({ name: loggedUser.email }));
+    }
+  };
+
+  const switchSignInPage = () => {
+    dispatch(authActions.signIn());
+  };
+
+  return (
+    <React.Fragment>
+      <h1 style={{ color: "#5b5b5b" }}>Sign Up</h1>
+      <Input
+        placeholder="Email"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Requirements
+        value={password}
+        requirements={passwordRequirements}
+        onValidChange={(isValid) => setValid(isValid)}
+      />
+
+      <Button
+        disabled={!valid || !username}
+        sx={{
+          margin: "15px 0px"
+        }}
+        text="sign Up"
         onClick={handleSignUp}
       />
-      <span style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontFamily: 'Arial',
-        fontSize: '13px'
-        }}>
-          Return to&nbsp; <Link onClick={switchSignInPage}>sign in</Link> &nbsp;page
+      <span
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontFamily: "Arial",
+          fontSize: "13px"
+        }}
+      >
+        Return to&nbsp; <Link onClick={switchSignInPage}>sign in</Link>{" "}
+        &nbsp;page
       </span>
     </React.Fragment>
-  )
-}
+  );
+};
 
 const AuthPage: React.FC = () => {
   const reduxAuth: any = useSelector((state: any) => state.auth);
 
-  return ( 
-  <Box>
-    {reduxAuth.signUp === false ? <Login /> : <SignUp />}
-  </Box>
-  )
-}
+  return <Box>{reduxAuth.signUp === false ? <Login /> : <SignUp />}</Box>;
+};
 
-export default AuthPage
+export default AuthPage;
--- packages/web/src/pages/home/Home.tsx
+++ packages/web/src/pages/home/Home.tsx
@@ -1,12 +1,17 @@
-import React from "react"
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 export default () => {
-    const user: any = useSelector((state:any) => state.auth)
+  const user: any = useSelector((state: any) => state.auth);
+  console.log(user, "user");
 
-    return (
-      <React.Fragment>
-        {user.user.name ? <h1>welcome {user.user.name}</h1> : <h1>user.displayName</h1>}
-      </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      {user.user.name ? (
+        <h1>welcome {user.user.name}</h1>
+      ) : (
+        <h1>user.displayName</h1>
+      )}
+    </React.Fragment>
+  );
+};
